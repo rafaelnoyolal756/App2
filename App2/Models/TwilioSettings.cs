6421816362af323fed56fbfef67fdbeab2c666a8
rafaelnoyolal756 <rafaelnoyola@gmail.com>
2025-01-09 01:20:28 -0600

    added solution to exam

---
+++ App2/Models/TwilioSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppUI1.Models
+{
+    public class TwilioSettings
+    {
+        public int Id { get; set; }
+        public string AccountSid { get; set; }
+        public string AuthToken { get; set; }
+    }
+}
